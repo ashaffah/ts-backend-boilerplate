e2fe86809147f003dd286294c40dc4774acfc5e5
ashaffah <wagamama.morph@gmail.com>
2026-02-08 06:22:22 +0700

    feat: extend BigInt interface with toJSON method for JSON serialization

--- src/core/config/app.config.ts
+++ src/core/config/app.config.ts
@@ -68,4 +68,25 @@ app.register(
   { prefix: "/api/v1" },
 );
 
+/** Prevent BigInt serialization issues
+ *  ref:
+ *  https://stackoverflow.com/a/78681416/13030162
+ *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt#use_within_json
+ */
+/**
+ * Extends the global `BigInt` interface to include a `toJSON` method.
+ *
+ * The `toJSON` method allows `BigInt` values to be serialized to JSON by converting them to a `number`.
+ * Note: This may result in loss of precision for large `BigInt` values that exceed the safe integer range.
+ */
+declare global {
+  interface BigInt {
+    toJSON(): number;
+  }
+}
+
+BigInt.prototype.toJSON = function () {
+  return Number(this.toString());
+};
+
 export { app };
