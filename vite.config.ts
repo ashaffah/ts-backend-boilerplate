3f4111b325bbcc24fe37010adb6d65736fa5d264
ashaffah <wagamama.morph@gmail.com>
2026-02-08 20:37:45 +0700

    feat: update package.json and tsconfig for Vitest integration; add vite.config.ts for testing configuration

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+
+  test: {
+    environment: "node",
+    globals: true,
+    include: ["src/**/*.test.ts", "src/**/*.spec.ts"],
+    exclude: ["node_modules", "dist"],
+  },
+});
