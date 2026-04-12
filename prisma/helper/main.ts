95241f4f3981b6ce923ef61202034f000e1941e6
ashaffah <wagamama.morph@gmail.com>
2026-04-12 17:27:21 +0700

    fix(prisma): update disconnection logic to use stateConfig for Prisma

--- prisma/helper/main.ts
+++ prisma/helper/main.ts
@@ -1,9 +1,17 @@
 import "../../src/core/polyfills/bigint";
-import { logger } from "../../src/core/config";
+import { logger, stateConfig } from "../../src/core/config";
+
+const prisma = stateConfig.prisma;
 
 async function main() {}
 
-main().catch((err) => {
-  logger.error("Error in helper: ", err);
-  process.exit(1);
-});
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+    logger.info("Helper disconnected successfully.");
+  })
+  .catch(async (err) => {
+    logger.error("Running Helper failed: ", err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- prisma/seeders/main.ts
+++ prisma/seeders/main.ts
@@ -1,9 +1,17 @@
 import "../../src/core/polyfills/bigint";
-import { logger } from "../../src/core/config";
+import { logger, stateConfig } from "../../src/core/config";
+
+const prisma = stateConfig.prisma;
 
 async function main() {}
 
-main().catch((err) => {
-  logger.error("Error in Seeders: ", err);
-  process.exit(1);
-});
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+    logger.info("Seeder disconnected successfully.");
+  })
+  .catch(async (err) => {
+    logger.error("Running Seeder failed: ", err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
