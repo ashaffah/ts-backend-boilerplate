0c6a5d041080a0051e92593d10395af14c7dd385
ashaffah <wagamama.morph@gmail.com>
2026-04-13 15:44:50 +0700

    feat(vite): configure tsconfigPaths plugin with loose option and project path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [
+    tsconfigPaths({
+      loose: true,
+      projects: ["./tsconfig.json"],
+    }),
+  ],
 
   test: {
     environment: "node",
